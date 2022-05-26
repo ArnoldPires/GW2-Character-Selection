cbe296cbdcbd6ea8a2d84c59a2b6faa78cbf82ee
ArnoldPires <arnold.pires92@gmail.com>
2022-05-25 23:35:33 -0400

    fixed video issues, made all characters selectable

--- js/main.js
+++ js/main.js
@@ -1,1 +1,84 @@
-//
+document.getElementById('dieselVan').onclick = gwDieselVan
+function gwDieselVan() {
+  document.getElementById('dieselVan').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/dieselVan.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('eveingaura').onclick = gweveingaura
+function gweveingaura() {
+  document.getElementById('eveingaura').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/eveingaura.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('leonilda').onclick = gwleonilda
+function gwleonilda() {
+  document.getElementById('leonilda').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/leonilda.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('leonildá').onclick = gwleonildá
+function gwleonildá() {
+  document.getElementById('leonildá').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/leonildá.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('lordNecomongr').onclick = gwlordNecomongr
+function gwlordNecomongr() {
+  document.getElementById('lordNecomongr').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/lordNecomongr.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('noire').onclick = gwnoire
+function gwnoire() {
+  document.getElementById('noire').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/noire.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('rambro').onclick = gwrambro
+function gwrambro() {
+  document.getElementById('rambro').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/rambro.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('rayden').onclick = gwrayden
+function gwrayden() {
+  document.getElementById('rayden').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/rayden.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('gandalfTheBlack').onclick = gwgandalfTheBlack
+function gwgandalfTheBlack() {
+  document.getElementById('gandalfTheBlack').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/gandalfTheBlack.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('alissa').onclick = gwalissa
+function gwalissa() {
+  document.getElementById('alissa').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/alissa.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('ironMan').onclick = gwironMan
+function gwironMan() {
+  document.getElementById('ironMan').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/ironMan.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}
+document.getElementById('ranger').onclick = gwranger
+function gwranger() {
+  document.getElementById('ranger').style.border = '2px solid rgb(188,190,132)';
+  document.querySelector('video').src="../video/ranger.mp4"
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat'
+}

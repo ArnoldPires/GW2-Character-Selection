df17548f3f3bf656ab71be0356825324a7430f2c
ArnoldPires <arnold.pires92@gmail.com>
2022-06-11 00:29:25 -0400

    more stuff

--- classesPage/js/classes.js
+++ classesPage/js/classes.js
@@ -4,54 +4,72 @@ function gwGuard() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/guardianBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Guardian"
+  document.querySelector('h1').style.color = "#5fbcd3"
 }
 document.getElementById('revenant').onclick = gwRev
 function gwRev() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/revenantBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Revenant"
+  document.querySelector('h1').style.color = "#860000"
 }
 document.getElementById('warrior').onclick = gwWar
 function gwWar() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/warriorBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Warrior"
+  document.querySelector('h1').style.color = "#c2a056"
 }
 document.getElementById('engineer').onclick = gwEngi
 function gwEngi() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/engineerBackground2.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Engineer"
+  document.querySelector('h1').style.color = "#ce7f4b"
 }
 document.getElementById('ranger').onclick = gwRan
 function gwRan() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/rangerBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Ranger"
+  document.querySelector('h1').style.color = "#94c34a"
 }
 document.getElementById('thief').onclick = gwThief
 function gwThief() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/thiefBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Thief"
+  document.querySelector('h1').style.color = "#89676d"
 }
 document.getElementById('elementalist').onclick = gwEle
 function gwEle() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/elementalistBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "contain"
+  document.querySelector('h1').innerText = "Elementalist"
+  document.querySelector('h1').style.color = "#f77a84"
 }
 document.getElementById('mesmer').onclick = gwMe
 function gwMe() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/mesmerBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Mesmer"
+  document.querySelector('h1').style.color = "#d532ff"
 }
 document.getElementById('necromancer').onclick = gwNecro
 function gwNecro() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/core/necromancerBackground.jpg)"; 
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Necromancer"
+  document.querySelector('h1').style.color = "#3e8e72"
 }
 //HOT Specs
 document.getElementById('dragonhunter').onclick = gwDh
@@ -59,54 +77,72 @@ function gwDh() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/dragonhunterBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Dragonhunter"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('herald').onclick = gwHer
 function gwHer() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/heraldBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "unset"
+  document.querySelector('h1').innerText = "Herald"
+  document.querySelector('h1').style.color = "#860000"
 }
 document.getElementById('berserker').onclick = gwBerk
 function gwBerk() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/berserkerBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "unset"
+  document.querySelector('h1').innerText = "Berserker"
+  document.querySelector('h1').style.color = "#c2a056"
 }
 document.getElementById('scrapper').onclick = gwScrap
 function gwScrap() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/scrapperBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Scrapper"
+  document.querySelector('h1').style.color = "#ce7f4b"
 }
 document.getElementById('druid').onclick = gwDru
 function gwDru() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/druidBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Druid"
+  document.querySelector('h1').style.color = "#94c34a"
 }
 document.getElementById('daredevil').onclick = gwDd
 function gwDd() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/daredevilBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Daredevil"
+  document.querySelector('h1').style.color = "#89676d"
 }
 document.getElementById('tempest').onclick = gwTemp
 function gwTemp() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/tempestBackground.jpg)"; 
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Tempest"
+  document.querySelector('h1').style.color = "#f77a84"
 }
 document.getElementById('chronomancer').onclick = gwChrono
 function gwChrono() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/chronomancerBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "unset"
+  document.querySelector('h1').innerText = "Chronomancer"
+  document.querySelector('h1').style.color = "#d532ff"
 }
 document.getElementById('reaper').onclick = gwReap
 function gwReap() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/hot/reaperBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Reaper"
+  document.querySelector('h1').style.color = "#3e8e72"
 }
 //POF Specs
 document.getElementById('firebrand').onclick = gwFb
@@ -114,54 +150,72 @@ function gwFb() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/firebrandBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Firebrand"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('renegade').onclick = gwRen
 function gwRen() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/renegadeBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Renegade"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('spellbreaker').onclick = gwSpell
 function gwSpell() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/spellbreakerBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Spellbreaker"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('holosmith').onclick = gwHolo
 function gwHolo() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/holosmithBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Holosmith"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('soulbeast').onclick = gwSoul
 function gwSoul() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/soulbeastBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Soulbeast"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('deadeye').onclick = gwDead
 function gwDead() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/deadeyeBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Deadeye"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('weaver').onclick = gwWeaver
 function gwWeaver() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/weaverBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Weaver"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('mirage').onclick = gwMirage
 function gwMirage() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/mirageBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Mirage"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('scourge').onclick = gwScourge
 function gwScourge() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/pof/scourgeBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Scourge"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 //EOD Specs
 document.getElementById('willbender').onclick = gwWill
@@ -169,52 +223,70 @@ function gwWill() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/willbenderBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Willbender"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('vindicator').onclick = gwVin
 function gwVin() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/vindicatorBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Vindicator"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('bladesworn').onclick = gwBlade
 function gwBlade() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/bladeswornBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "contain"
+  document.querySelector('h1').innerText = "Bladesworn"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('mechanist').onclick = gwMech
 function gwMech() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/mechanistBackground.png)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Mechanist"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('untamed').onclick = gwTamed
 function gwTamed() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/untamedBackground.png)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Untamed"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('specter').onclick = gwSpec
 function gwSpec() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/specterBackground.png)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Specter"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('catalyst').onclick = gwCala
 function gwCala() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/catalystBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "top"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Catalyst"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('virtuoso').onclick = gwVirt
 function gwVirt() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/virtuosoBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "unset"
+  document.querySelector('h1').innerText = "Virtuoso"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
 document.getElementById('harbinger').onclick = gwHarb
 function gwHarb() {
   document.querySelector('body').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(../../img/specBackground/eod/harbingerBackground.jpg)";
   document.querySelector('body').style.backgroundPosition = "center"
   document.querySelector('body').style.backgroundSize = "cover"
+  document.querySelector('h1').innerText = "Harbinger"
+  document.querySelector('h1').style.color = "#8AF7F5"
 }
